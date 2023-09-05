283c181fa1906ae6badf857329bebe0133c02706
Liam Pendleton <pendle@uw.edu>
2023-09-04 22:04:37 -0700

    New data; implemented some datasets into the nest model.

--- Scripts/covariate_correlation.R
+++ Scripts/covariate_correlation.R
@@ -9,28 +9,29 @@ sst <- read.csv(here("Data", "racerocks_mSST.csv"))
 npgo <- read.csv(here("Data", "NPGO.csv"))                
 pdo <- read.csv(here("Data", "PDO.csv"))
 
-head(sst)
 sst[sst == 999.99] <- NA #wouldn't want a value like that guiding our analyses
-pi_sst <- sst[which(sst$YEAR >= 1995),] #isolate years of interest
+pi_full_sst <- sst[which(sst$YEAR >= 1995),] #isolate years of interest
+pi_pre_sst <- pi_full_sst[,1:6] #pre-breeding season defined as Jan. 1 - May 31
 
-pi_npgo <- npgo[which(npgo$YEAR >= 1995),]
+pi_full_npgo <- npgo[which(npgo$YEAR >= 1995),]
+pi_pre_npgo <- pi_full_npgo[which(pi_full_npgo$MONTH <= 5),]
 
-pi_pdo <- pdo[which(pdo$Year >= 1995),]
+pi_full_pdo <- pdo[which(pdo$Year >= 1995),]
+pi_pre_pdo <- pi_full_pdo[,1:6]
 
-data <- array()
 #############################################
 #pi_sst and pi_pdo have the same dimensions, allowing me to check them like this. 
 #pi_npgo does not have the same dimensions, so i'm running into trouble using this command to check for correlation.
 
-cor1<- cor(x = pi_sst, y = pi_pdo,
+cor_sst_pdo <- cor(x = pi_full_sst, y = pi_full_pdo,
     use = "complete.obs", method = "pearson") #using complete.obs uses casewise deletion in the event of missing values/NAs
 
-cor2 <- cor(x = pi_sst, y = pi_pdo,
+cor2 <- cor(x = pi_full_sst, y = pi_full_pdo,
     use = "na.or.complete", method = "pearson") #same output as above; it is erring on side of complete obs 
 
-cor3 <- cor(x = pi_sst, y = pi_pdo,
-             use = "pairwise.complete.obs", method = "pearson") #corr calculated based on all compelete pairs... not sure what is fundamentally different, but output differs.
+cor3 <- cor(x = pi_full_sst, y = pi_full_pdo,
+             use = "pairwise.complete.obs", method = "pearson") #corr calculated based on all complete pairs... not sure what is fundamentally different, but output differs.
 
 
-ggcorrplot(cor1)
-ggcorrplot(cor3)
+ggcorrplot(cor_sst_pdo)
+ggcorrplot(cor3) 
--- Scripts/nest_model.R
+++ Scripts/nest_model.R
@@ -1,5 +1,3 @@
-library(here)
-library(rjags)
 
 
 #read in data 
@@ -17,22 +15,36 @@ for(i in 1:n.nests){
   omega[i,2] <- S[i]*(1-gamma[i])   #1 egg
   omega[i,3] <- S[i]*gamma[i]   #2 eggs 
 
-  logit(S[i]) <- int.S + eps.S[year[i]]
-  logit(gamma[i]) <- int.gam + eps.gam[year[i]]
+  logit(S[i]) <- int.S + eps.S[year[i]] + eps.sst[year[i]] + eps.pdo[year[i]] + eps.npgo[year[i]] #LP
+  logit(gamma[i]) <- int.gam + eps.gam[year[i]] + eps.sst[year[i]] + eps.pdo[year[i]] + eps.npgo[year[i]] #LP
+
 } 
 
 for(y in 1:n.years){  #random effects to explain residual annual variation 
   eps.S[y] ~ dnorm(0,tau.S) 
   eps.gam[y] ~ dnorm(0,tau.gam)
+  eps.sst[y] ~ dnorm(0, tau.sst) #LP
+  #eps.chla[y] ~ dnorm(0, tau.chla) #LP
+  eps.pdo[y] ~ dnorm(0, tau.pdo) #LP
+  eps.npgo[y] ~ dnorm(0, tau.npgo) #LP
 } 
 
 tau.S <- pow(sigma.S,-2)
 sigma.S ~ dunif(0,10) 
 tau.gam <- pow(sigma.gam,-2)
-sigma.gam ~ dunif(0,10) 
+sigma.gam ~ dunif(0,10)
+
+tau.sst <- pow(sigma.sst,-2) #LP
+sigma.sst <- dunif(0,10) #LP 
+#tau.chla <- pow(sigma.chla,-2) #LP
+#sigma.chla <- dunif(0,10) #LP
+tau.pdo <- pow(sigma.pdo,-2) #LP
+sigma.pdo <- dunif(0,10) #LP
+tau.npgo <- pow(sigma.npgo,-2) #LP
+sigma.npgo <- dunif(0,10) #LP
 
 int.S ~ dnorm(0,1) 
-int.gam ~ dnorm(0,1) 
+int.gam ~ dnorm(0,1)
 
 mean.S <- 1/(1+exp(-(int.S)))
 
@@ -46,7 +58,7 @@ nests$year <- nests$year - 1995
 nests$year <- as.numeric(as.factor(nests$year))
 
 
-data<-list(y = nests$outcome, year = nests$year, n.nests = dim(nests)[1], n.years = length(unique(nests$year)))
+data<-list(y = nests$outcome, year = nests$year, n.nests = dim(nests)[1], n.years = length(unique(nests$year)), sst = pi_full_sst, pdo = pi_full_pdo, npgo = pi_full_npgo)
 
 parameters<-c('int.S','mean.S')
 
