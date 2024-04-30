931d3923514fa9edbc6680794e3bfa3719079729
Liam Pendleton <99692114+liampendleton@users.noreply.github.com>
2024-04-30 13:09:11 -0700

    Add files via upload
    I don't think much actually changed here... just incase.

--- Scripts/results/figures.R
+++ Scripts/results/figures.R
@@ -11,7 +11,7 @@ df.gam$Model <- factor(df.survival$Model, levels = custom_order)
 # Create plot for survival across models
 ggplot(df.survival, aes(x = Model, y = Mean)) +
   geom_point(size = 3, show.legend = FALSE, color = "red2") +
-  geom_errorbar(aes(ymin = Lower_CrI, ymax = Upper_CrI), width = 0.1, size = 1, color = "red2", show.legend = FALSE) + #credible interavals
+  geom_errorbar(aes(ymin = Lower_CrI, ymax = Upper_CrI), width = 0.1, linewidth = 1, color = "red2", show.legend = FALSE) + #credible interavals
   labs(x = bquote(plain("Models")), y = expression(plain(paste("Mean ", phi, " (95% CrI)")))) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 12, face = "plain"),
         axis.text.y = element_text(size = 12, face = "plain"),
@@ -26,7 +26,7 @@ ggplot(df.survival, aes(x = Model, y = Mean)) +
 # Create plot for production across models
 ggplot(df.gam, aes(x = Model, y = Mean, color = "red2")) +
   geom_point(size = 3, show.legend = FALSE, color = "red2") +
-  geom_errorbar(aes(ymin = Lower_CrI, ymax = Upper_CrI), width = 0.1, size = 1, show.legend = FALSE, color = "red2") + #credible interavals
+  geom_errorbar(aes(ymin = Lower_CrI, ymax = Upper_CrI), width = 0.1, linewidth = 1, show.legend = FALSE, color = "red2") + #credible interavals
   labs(x = bquote(plain("Models")), y = expression(plain(paste("Mean ", gamma, " (95% CrI)")))) +
   theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 12, face = "plain"),
         axis.text.y = element_text(size = 12, face = "plain"),
@@ -58,6 +58,28 @@ ggplot(df_pred, aes(x = NPGO, y = Mean)) +
   geom_line(size = 1, color = "red") + # Line connecting mean values
   geom_ribbon(aes(ymin = Lower_CrI, ymax = Upper_CrI), fill = "#FF6F61", alpha = 0.3) + # Uncertainty ribbon
   labs(x = expression(plain("NPGO")), y = expression(plain(paste("Mean ", phi, " (95% CrI)")))) +
+  theme(axis.text.x = element_text(hjust = 1, size = 12, face = "plain"),
+        axis.text.y = element_text(size = 12, face = "plain"),
+        axis.title.x = element_text(size = 15),
+        axis.title.y = element_text(size = 15),
+        panel.background = element_rect(fill = "white"),
+        panel.grid.major = element_line(color = "gray90"),
+        panel.grid.minor = element_line(color = "gray90")) +
+  scale_y_continuous(limits = c(0,1))
+
+#############################################################
+# Plot random deviates by year
+years.pred.hist <- seq(1996, 2013, by = 1)
+years.pred.pres <- c(2022, 2023)
+years.pred <- c(years.pred.hist, years.pred.pres)
+deviates_year <- list(years = years.pred, Mean = out.bestmodel, Lower_CrI = Cr.pred[1,], Upper_CrI = Cr.pred[2,])
+
+
+# Create the plot using ggplot2
+ggplot(df_pred, aes(x = NPGO, y = Mean)) +
+  geom_line(size = 1, color = "red") + # Line connecting mean values
+  geom_ribbon(aes(ymin = Lower_CrI, ymax = Upper_CrI), fill = "#FF6F61", alpha = 0.3) + # Uncertainty ribbon
+  labs(x = expression(plain("Year")), y = expression(plain(paste("Mean ", phi, " (95% CrI)")))) +
   theme(axis.text.x = element_text(hjust = 1, size = 12, face = "plain"),
         axis.text.y = element_text(size = 12, face = "plain"),
         axis.title.x = element_text(size = 15),
