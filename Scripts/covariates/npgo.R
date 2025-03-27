55bd8c04f6fb096808be715c6c387782ea3ea0e9
Liam Pendleton <pendle@uw.edu>
2025-03-27 16:52:14 -0700

    Minor changes. Not really meaningful.

--- Scripts/covariates/npgo.R
+++ Scripts/covariates/npgo.R
@@ -1,14 +1,11 @@
-#Import NPGO data directly from source
-url <- "https://www.o3d.org/npgo/data/NPGO.txt"
-
 #Create a function that can be called on to process and show average annual NPGO values from 1995-2023
-npgo.fxn <- function(){
-  npgo <- read.table(url,
+# npgo.fxn <- function(){
+  npgo <- read.table("https://www.o3d.org/npgo/data/NPGO.txt", #Download directly from source
                      skip = 1,
                      col.names = c("Year", "Month", "NPGO"))
   #NPGO setup
   pi_full_npgo <- npgo[which(npgo$Year >= 1995 & npgo$Year != 2025),] #Isolate period of interest; 1995-2024
-  npgo_vec <- pi_full_npgo[-c(1:4, 893:900),3] #Remove January through April of 1995 and any records after April of 2024
+  npgo_vec <- pi_full_npgo[-c(1:4, 898:900),3] #Remove January through April of 1995 and any records after Sept of 2024
   
   #Address different timescales
   npgo.1 <- npgo.2 <- npgo.3 <- npgo.4 <- npgo.5 <- rep(NA,29) #29 annual averages 
